95e7c426a0384e2f0d0d0401f8cb8e7f9e22f56c
aproposmath <aproposmath@proton.me>
2025-08-25 11:53:29 +0200

    Num to string conversion example

---
+++ examples/string_counter.py
@@ -0,0 +1,38 @@
+from stationeers_pytrapic.symbols import *
+
+
+def append_number(num_string, n):
+    if n == 0:
+        return 256 * num_string + 48
+    d = 1
+    while d <= n / 10:
+        d *= 10
+    db.Setting = d
+    while d >= 1:
+        round(d, d)
+        digit = n / d
+        trunc(digit, digit)
+        num_string *= 256
+        num_string += digit
+        num_string += 48
+        n -= digit * d
+        d = d / 10
+    return num_string
+
+
+num0 = 0
+num1 = 50
+
+display = ConsoleLED1x3s
+display.Mode = 10
+
+while True:
+    yield_()
+    num_string = 0
+    num_string = append_number(num_string, num0)
+    num_string *= 256
+    num_string += 47  # ASCII for '/'
+    num_string = append_number(num_string, num1)
+    display.Setting = num_string
+
+    num0 += 1
