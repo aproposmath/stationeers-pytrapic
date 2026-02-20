94818f35e3e7042d4ae3f0ed64900e6c30152edd
aproposmath <aproposmath@proton.me>
2026-02-20 17:00:21 +0100

    Get rid of StationeersLaunchpad dependency

--- mod/Installer.cs
+++ mod/Installer.cs
@@ -5,8 +5,7 @@ using System.IO.Compression;
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine.Networking;
-
-using StationeersLaunchPad;
+using Newtonsoft.Json;
 
 namespace StationeersPyTrapIC;
 
@@ -34,51 +33,54 @@ public class PythonWorkspace
     public static bool IsWorkspaceInitialized => File.Exists(WorkspaceVersionFile) && File.ReadAllText(WorkspaceVersionFile).Trim() == WorkspaceVersionTag;
 
     private static readonly SemaphoreSlim _initLock = new(1);
-    
-    private static async UniTask<ZipArchive> FetchZip(Github.Asset asset)
+
+    private static async UniTask<ZipArchive> FetchZip(string url)
     {
-      using (var downloadRequest = UnityWebRequest.Get(asset.BrowserDownloadUrl))
-      {
-        L.Info($"Downloading {asset.Name}");
-        var downloadResult = await downloadRequest.SendWebRequest();
-
-        if (downloadResult.result != UnityWebRequest.Result.Success)
+        using (var downloadRequest = UnityWebRequest.Get(url))
         {
-          L.Error($"Failed to download {asset.Name}! result: {downloadResult.result}, error: {downloadResult.error}");
-          return null;
+            L.Info($"Downloading {url}");
+            var downloadResult = await downloadRequest.SendWebRequest();
+
+            if (downloadResult.result != UnityWebRequest.Result.Success)
+            {
+                L.Error($"Failed to download {url}! result: {downloadResult.result}, error: {downloadResult.error}");
+                return null;
+            }
+
+            var data = downloadRequest.downloadHandler.data;
+            L.Debug($"Downloaded {data.Length} bytes");
+            var stream = new MemoryStream(data.Length);
+            stream.Write(data, 0, data.Length);
+            stream.Position = 0;
+
+            return new ZipArchive(stream);
         }
-
-        var data = downloadRequest.downloadHandler.data;
-        L.Debug($"Downloaded {data.Length} bytes");
-        var stream = new MemoryStream(data.Length);
-        stream.Write(data, 0, data.Length);
-        stream.Position = 0;
-
-        return new ZipArchive(stream);
-      }
     }
 
 
     public static async UniTask FetchAndExtract(string versionTag, string assetName, string extractDir)
     {
-        Github.Repo repo = new("aproposmath", "stationeers-pytrapic");
-        var release = await repo.FetchTagRelease(versionTag);
-        if (release == null)
+        var url = $"https://api.github.com/repos/aproposmath/stationeers-pytrapic/releases/tags/{versionTag}";
+        using var request = UnityWebRequest.Get(url);
+        request.SetRequestHeader("User-Agent", "UnityWebRequest");
+        var response = await request.SendWebRequest();
+
+        if (response.result != UnityWebRequest.Result.Success)
         {
-            L.Error($"Could not find release for tag {versionTag}");
-            throw new Exception($"Could not find release for tag {versionTag}");
+            L.Error($"Failed to fetch release info! result: {response.result}, error: {response.error}");
+            throw new Exception($"Failed to fetch release info for tag {versionTag}");
         }
 
-        var asset = release.Assets.Find(a => a.Name == assetName);
-        if (asset == null)
+        var json = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(request.downloadHandler.text);
+        var assetUrl = (string)json.SelectToken($"$.assets[?(@.name == '{assetName}')].url");
+
+        if (assetUrl == null)
         {
-            L.Error("Could not find ws.zip asset in release");
-            throw new Exception("Could not find ws.zip asset in release");
+            L.Error($"Could not find asset ${assetName} in release ${versionTag}");
+            throw new Exception($"Could not find asset ${assetName} in release ${versionTag}");
         }
 
-        L.Info($"Downloading {asset.Name} from {repo.Owner}/{repo.Name}...");
-
-        var archive = await FetchZip(asset);
+        var archive = await FetchZip(assetUrl);
         archive.ExtractToDirectory(extractDir);
     }
 
